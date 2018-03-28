2aa89b7ad1e1cf177d0bae531b29a52bbfc336b0
samidhothar <sami.03318207979@gmail.com>
2018-03-29 03:09:48 +0500

    initial Release
    First Commit

---
+++ app/src/main/java/com/delaroystudios/firebasechat/APICalls.java
@@ -0,0 +1,20 @@
+package com.delaroystudios.firebasechat;
+
+/**
+ * Created by api on 19/2/16.
+ */
+
+import retrofit.Call;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+public interface APICalls {
+
+
+    @FormUrlEncoded
+    @POST("/api/v1.5/tr.json/translate")
+    Call<TranslateResponse> translate(@Field("key") String key, @Field("text") String text, @Field("lang") String lang);
+
+
+}
